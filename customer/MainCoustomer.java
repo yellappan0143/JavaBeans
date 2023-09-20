eec3700236ec07ab90ae0c72e9c74a56ef10650d
YELLAPPAN P <109733281+Madesh0143@users.noreply.github.com>
2023-09-20 21:10:20 +0530

    Update MainCoustomer.java
    To accessing data from different  class providing security

--- customer/MainCoustomer.java
+++ customer/MainCoustomer.java
@@ -2,23 +2,19 @@ package com.java.customer;
 
 public class MainCoustomer 
 {
-
 	public static void main(String[] args) 
 	{
 		Customer C = new Customer();
-		     C.setName("Lance");
-		     C.setEmail_id("lance123@gmail.com");
-		     C.setPhone(7894561230l);
-		     C.setSalary(199.0);
+			 C.setName("Lance");
+			 C.setEmail_id("lance123@gmail.com");
+			 C.setPhone(7894561230l);
+			 C.setSalary(199.0);
 		     
-		     System.out.println("Customer Details Using 'JAVA BEANS'");
-		     System.out.println("-------------------------------------");
-		     System.out.println("Name : "+C.getName());
-		     System.out.println("Email Id : "+C.getEmail_id());
-		     System.out.println("Phone No : "+C.getPhone());
-		     System.out.println("Salary : "+C.getSalary());
-		     
-
+	        System.out.println("Customer Details Using 'JAVA BEANS'");
+	        System.out.println("-------------------------------------");
+	        System.out.println("Name : "+C.getName());
+	        System.out.println("Email Id : "+C.getEmail_id());
+	        System.out.println("Phone No : "+C.getPhone());
+	        System.out.println("Salary : "+C.getSalary());
 	}
-
 }
